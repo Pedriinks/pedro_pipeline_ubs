babed5a8aa042c918daf9fdbe151b955c590b1c3
Pedriinks <126614120+Pedriinks@users.noreply.github.com>
2025-02-27 01:38:42 +0000

    Grafico de Pizza Corrigido

--- ubs_grafico_pizza.py
+++ ubs_grafico_pizza.py
@@ -5,11 +5,10 @@ import plotly.express as px
 df = pd.read_csv('ubs_atualizado.csv', sep = ";")
 
 valores = pd.DataFrame({
-    "Estados": df['Nome_UF'],
-    "Porcentagem": df['UF'].value_counts()
+    "Estados": df['Nome_UF']
 })
 
 #Grafico de Pizza
-grafico = px.pie(valores, names = "Estados", values = "Porcentagem", title = "Grafico")
+grafico = px.pie(valores, names = "Estados", title = "Grafico")
 
 st.plotly_chart(grafico)
